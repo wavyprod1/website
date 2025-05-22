746f1835accc5d7d4b782d4fa9dec7a18e43a75f
Elia Pirrello <wavypersonal1@gmail.com>
2025-05-22 02:50:00 +0200

    Preparo per il deploy

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,12 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: '/website/',
 })
